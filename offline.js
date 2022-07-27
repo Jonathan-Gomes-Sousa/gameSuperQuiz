c502e9622619a2cac9f6ba477ecc41367c33da64
Jonathan Gomes <69915159+Jonathan-Gomes-Sousa@users.noreply.github.com>
2022-07-27 15:39:24 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1658950812,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/quadro_negro-sheet0.png",
+		"images/vidas-sheet0.png",
+		"images/acerto-sheet0.png",
+		"images/acerto-sheet1.png",
+		"images/loading-sheet0.png",
+		"images/menino-sheet0.png",
+		"images/lapis-sheet0.png",
+		"images/btn_menu-sheet0.png",
+		"images/btn_retry-sheet0.png",
+		"images/logo-sheet0.png",
+		"images/menu_icones-sheet0.png",
+		"media/correct.ogg",
+		"media/error.ogg",
+		"media/gameover.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"perguntas.xml"
+	]
+}
